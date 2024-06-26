fc5177e9f950b61ae9efbc2cc80abb5c78057f0a
Angel0603 <20211050@uthh.edu.mx>
2024-06-26 05:19:25 -0600

    Actualizacion

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, render_template, jsonify
 import pandas as pd
 from tensorflow.keras.models import load_model
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Cargar el modelo entrenado (Asegúrate de que el modelo está guardado en formato adecuado para Keras)
 model = load_model('modelo_paris.h5')
